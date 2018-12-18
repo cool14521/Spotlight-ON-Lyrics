1a09901f8ec08e5b16940776f5fc4ee56e01fd94
YutaKanna <kannayuta0629@gmail.com>
2018-12-19 00:14:36 +0900

    新規登録、ログイン、パスワードリセット機能の追加

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    return view('layouts/app');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('auth/register', 'Auth\RegisterController@register');
+
+Route::get('/auth/login', 'Auth\LoginController@showLoginForm');
+Route::post('/auth/login', 'Auth\LoginController@login');
+Route::get('/auth/logout', 'Auth\LoginController@logout');
