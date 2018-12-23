76324b51e14c24d7e6584a3cbe0158d1560f4daa
YutaKanna <kannayuta0629@gmail.com>
2018-12-23 18:37:25 +0900

    リダイレクト先をルートディレクトリに変更する

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -43,6 +43,6 @@ class LoginController extends Controller
 
         $request->session()->invalidate();
 
-        return $this->loggedOut($request) ?: redirect('/home');
+        return $this->loggedOut($request) ?: redirect('/');
     }
 }
