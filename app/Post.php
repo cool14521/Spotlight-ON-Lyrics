eeb9e0c19cc076eec8159abf8711e0afd2a90be3
YutaKanna <kannayuta0629@gmail.com>
2018-12-23 18:43:18 +0900

    リレーションを追加する

--- app/Post.php
+++ app/Post.php
@@ -7,11 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     /**
-     * リレーション (従属の関係)
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function user() // 単数形
+    public function user()
     {
         return $this->belongsTo('App\User');
     }
