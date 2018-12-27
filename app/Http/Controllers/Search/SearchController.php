7c7263d5e484b1bfceff28d081aab20e8397c00b
YutaKanna <kannayuta0629@gmail.com>
2018-12-27 19:47:57 +0900

    歌詞を探すページの作成

--- app/Http/Controllers/Search/SearchController.php
+++ app/Http/Controllers/Search/SearchController.php
@@ -14,6 +14,6 @@ class SearchController extends Controller
      */
     public function index()
     {
-        return view('search.index');
+        return view('searches.index');
     }
 }
--- resources/views/searches/index.blade.php
+++ resources/views/searches/index.blade.php
@@ -1,1 +1,29 @@
-something
+@extends('layouts.app')
+@section('title', 'Spotlight-ON-Lyrics')
+@section('content')
+    <div class="container"></div>
+        <div class="inner cover">
+            <h1 class="py-5">Cover your page.</h1>
+            <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+            <p class="lead">
+                <div class="container-fluid">
+                    <div class="row">
+                        <a class="col-lg-4 btn btn-danger btn-lg" href="#" role="button">新曲</a>
+                        <a class="col-lg-4 btn btn-warning btn-lg" href="#" role="button">歌手名</a>
+                        <a class="col-lg-4 btn btn-success btn-lg" href="#" role="button">曲名</a>
+                    </div>
+                    <div class="row">
+                        <a class="col-lg-4 btn btn-primary btn-lg" href="#" role="button">人気曲</a>
+                        <a class="col-lg-4 btn btn-dark btn-lg" href="#" role="button">ジャンル</a>
+                        <a class="col-lg-4 btn btn-info btn-lg" href="#" role="button">りれき</a>
+                    </div>
+                    <div class="row">
+                        <a class="col-lg-4 btn btn-secondary btn-lg" href="#" role="button">キーワード</a>
+                        <a class="col-lg-4 btn btn-outline-danger btn-lg" href="#" role="button">リリース年別</a>
+                        <a class="col-lg-4 btn btn-primary rounded-pill btn-lg" href="#" role="button">オススメ</a>
+                    </div>
+                </div>
+            </p>
+        </div>
+    </div>
+@endsection
